a989f284e33750b51aee693487321ec90b0fb8fb
OstapUrdey <43811378+OstapUrdey@users.noreply.github.com>
2024-10-09 16:05:32 +0300

    finish homework ts

---
+++ src/generics/4.ts
@@ -0,0 +1,17 @@
+// type User = {
+//     name: string;
+//     surname: string;
+//     email: string;
+//     password: string;
+// };
+
+// function createOrUpdateUser(initialValues: Partial<User>): User {
+//     const defaultUser: User = {
+//         name: '',
+//         surname: '',
+//         email: 'user@mail.com', 
+//         password: 'password123',
+//     }
+
+//     return { ...defaultUser, ...initialValues };
+// };
