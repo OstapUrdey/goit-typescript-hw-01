b854dd46f97d7b39ab7938ea4e3575c7d31299ff
OstapUrdey <43811378+OstapUrdey@users.noreply.github.com>
2024-10-09 12:10:04 +0300

    basic types - closed

---
+++ src/basic/3.ts
@@ -0,0 +1,9 @@
+// let mixedType: string | number;
+
+// mixedType = "string";
+// mixedType = 100;
+
+// let EnableOrDisable: "enable" | "disable";
+
+// EnableOrDisable = "enable";
+// EnableOrDisable = "disable";
